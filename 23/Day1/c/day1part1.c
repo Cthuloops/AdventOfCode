783886d9d5686048a9c750de2e715d6865372884
hcoughlin <104121493+Cthuloops@users.noreply.github.com>
2024-02-25 08:23:33 -0500

    works on small data set but broken on large

---
+++ 23/Day1/c/day1part1.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int asciiConversion(int asciiNumber) {
+    int numToConvert = asciiNumber;
+    int convertedNum;
+
+    switch (numToConvert) {
+        case 48:
+            convertedNum = 0;
+            break;
+        case 49:
+            convertedNum = 1;
+            break;
+        case 50:
+            convertedNum = 2;
+            break;
+        case 51:
+            convertedNum = 3;
+            break;
+        case 52:
+            convertedNum = 4;
+            break;
+        case 53:
+            convertedNum = 5;
+            break;
+        case 54:
+            convertedNum = 6;
+            break;
+        case 55:
+            convertedNum = 7;
+            break;
+        case 56:
+            convertedNum = 8;
+            break;
+        case 57:
+            convertedNum = 9;
+            break;
+    }
+
+    return convertedNum;
+}
+
+int twoDigitConversion(char nextLine[]) {
+    const int LENGTH = strlen(nextLine);
+    int num1 = 0, num2 = 0, twoDigit = 0;
+    bool num1found = false, num2found = false;
+
+    for (int i = 0; i < LENGTH; i++) {
+        if (nextLine[i] >= 48 && nextLine[i] <= 57) {
+            if (!num1found) {
+                num1 = asciiConversion(nextLine[i]);
+                num1found = true;
+            } else if (nextLine[i] >= 48 && nextLine[i] <= 57 && num1found) {
+                num2 = asciiConversion(nextLine[i]);
+                num2found = true;
+            }
+        }
+    }
+    if (num1found && !num2found) {
+        num2 = num1;
+    }
+    twoDigit = (num1 * 10) + num2;
+    return twoDigit;
+}
+
+
+int main() {
+    
+    int total = 0;
+    char nextLine[30];
+
+    //open file pointer to text file containing information
+    FILE *fptr;    
+    fptr = fopen("../file.txt", "r");
+
+    if (fptr != NULL) {
+        while(fgets(nextLine, 30, fptr)) {
+            total += twoDigitConversion(nextLine);
+        }
+    } else {
+        printf("Not able to open file.");
+    }
+
+    fclose(fptr);
+
+    printf("Total: %d\n", total);
+    return 0;
+}
