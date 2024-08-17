d637d6a502267f591961dafa3d6524a2d51c0b11
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2024-08-17 12:17:53 -0400

    got the example to run correctly

---
+++ 23/Day1/cpp/part1.cpp
@@ -0,0 +1,48 @@
+#include <cctype>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout, std::endl, std::string;
+
+
+int extractNumber(string* pLine) {
+    int firstDigit, secondDigit;
+    bool firstFound = false;
+    bool secondFound = false;
+    for (char character : *pLine) {
+        // 48 is ascii code for 0
+        if (isdigit(character) && !firstFound) {
+            firstDigit = character - 48;
+            firstFound = true;
+        } else if (isdigit(character) && firstFound) {
+            secondDigit = character - 48;
+            secondFound = true;
+        }
+    }
+    if (firstFound && !secondFound) {
+        secondDigit = firstDigit;
+    }
+    int returnVal = ((firstDigit * 10) + secondDigit);
+    return returnVal;
+}
+
+
+int main() {
+    using std::ifstream;
+
+    cout << "Reading file" << endl;
+    ifstream File("../part1example.txt");
+
+    string Line;
+    int total = 0;
+    while (getline(File, Line)) {
+        string* pLine = &Line;
+        int num = extractNumber(pLine);
+        total += num;
+    }
+    File.close();
+    cout << total << endl;
+
+    return 0;
+}
