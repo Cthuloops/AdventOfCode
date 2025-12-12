b564833269ba240ad7cedca77cd81761bca7c4fa
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-12-11 19:17:02 -0500

    feat(25/Day4): part 2 complete. 1 shot both parts

--- 25/Day4/c/main.c
+++ 25/Day4/c/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // #define EXAMPLE
 
@@ -8,46 +9,51 @@
 #define filename "../example.txt"
 #define rows 10
 #define cols 10
-#define length 12
 #else
 #define filename "../input.txt"
 #define rows 136
 #define cols 136
-#define length 138
 #endif
 
 #define OPEN 4
 
-int check_neighbors(char buf[rows][length], int row, int col);
-int part1(char buf[rows][length]);
-// int part2(void);
+struct Cell {
+    bool can_be_removed;
+    char val;
+};
 
-void remove_newline(char buf[length]) {
-    char *str = buf;
-    if ((str = strchr(str, '\n')) != NULL) {
-        *str = '\0';
-    }
-}
+int check_neighbors(struct Cell cells[rows][cols], int row, int col);
+int part1(struct Cell cells[rows][cols]);
+int part2(struct Cell cells[rows][cols]);
 
 int main(void) {
-    char buf[rows][length];
+    char buf[BUFSIZ];
+    struct Cell cells[rows][cols];
 
     FILE *fp = fopen(filename, "r");
 
-    int i = 0;
-    while (fgets(buf[i], length, fp)) {
-        remove_newline(buf[i++]);
+    int r = 0, c;
+    while (fgets(buf, BUFSIZ, fp)) {
+        for (c = 0; c < cols; c++) {
+            cells[r][c] = (struct Cell) {
+                .can_be_removed = false,
+                .val = buf[c]
+            };
+        }
+        r++;
     }
 
     fclose(fp);
 
-    int total = part1(buf);
-    printf("%d\n", total);
+    int total = part1(cells);
+    printf("Part1: %d\n", total);
+    int removed = part2(cells);
+    printf("Part2: %d\n", removed);
 
     return 0;
 }
 
-int check_neighbors(char buf[rows][length], int row, int col) {
+int check_neighbors(struct Cell cells[rows][cols], int row, int col) {
     int rolls = 0;
     int y, x;
     for (y = row - 1; y <= row + 1; y++) {
@@ -60,7 +66,7 @@ int check_neighbors(char buf[rows][length], int row, int col) {
             // If we're at a boundary of the grid, it's free.
             if (y < 0 || x < 0 || y >= rows || x >= cols) {
                 continue;
-            } else if (buf[y][x] == '@') {
+            } else if (cells[y][x].val == '@') {
                 rolls++;
             }
         }
@@ -68,23 +74,41 @@ int check_neighbors(char buf[rows][length], int row, int col) {
     return rolls;
 }
 
-int part1(char buf[rows][length]) {
+int part1(struct Cell cells[rows][cols]) {
     int total = 0;
 
     int i, j;
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
-            if (buf[i][j] == '@') {
-                if (check_neighbors(buf, i, j) < OPEN) {
+            if (cells[i][j].val == '@') {
+                if (check_neighbors(cells, i, j) < OPEN) {
                     total++;
+                    cells[i][j].can_be_removed = true;
                 }
             }
         }
     }
 
-    // for (i = 0; i < rows; i++) {
-    //     printf("%s\n", buf[i]);
-    // }
-
     return total;
+}
+
+int part2(struct Cell cells[rows][cols]) {
+    int can_be_removed;
+    int removed = 0;
+
+    int i, j;
+    do {
+        can_be_removed = part1(cells);
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < cols; j++) {
+                if (cells[i][j].can_be_removed) {
+                    cells[i][j].can_be_removed = false;
+                    cells[i][j].val = 'x';
+                    removed++;
+                }
+            }
+        }
+    } while (can_be_removed > 0);
+
+    return removed;
 }
