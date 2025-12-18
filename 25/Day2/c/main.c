59d790f9ec049437970d8a999b63aab3b8312b51
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-12-18 16:32:16 -0500

    feat(25/Day2): part 1 complete

--- 25/Day2/c/main.c
+++ 25/Day2/c/main.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-// #define INPUT
-#ifdef INPUT
-#define filename "../input.txt"
+// #define EXAMPLE
+#ifdef EXAMPLE
+#define filename ("../example.txt")
+#define answer (1227775554)
 #else
-#define filename "../example.txt"
+#define filename ("../input.txt")
 #endif
 
-void remove_newline(char *);
-void lltostr(long long, char *, size_t);
+#define DECIMAL_BASE (10)
+
+void remove_newline(char *buf);
+size_t lltostr(uint64_t num, char *buf, size_t buf_size);
+uint64_t part1(uint64_t num1, uint64_t num2);
 
 int main(void) {
 
@@ -22,21 +27,26 @@ int main(void) {
         exit(1);
     }
 
-    fgets(buf, BUFSIZ, fp);
-    remove_newline(buf);
+    char *token;
+    uint64_t num1, num2;
+    uint64_t total = 0;
+    char *end;
+    while (fgets(buf, BUFSIZ, fp)) {
+        remove_newline(buf);
 
-    char *token = strtok(buf, ",");
-    long long num1, num2;
-    char num_str[BUFSIZ] = { 0 };
-    while (token) {
-        errno = 0;
-        num1 = strtoll(token, NULL, 10);
-        printf("Strtoll: %lld\n", num1);
-        lltostr(num1, num_str, BUFSIZ);
-        printf("Lltostr: %s\n\n", num_str);
-        token = strtok(NULL, ",");
+        token = strtok(buf, ",");
+        while (token) {
+            num1 = strtoull(token, &end, DECIMAL_BASE);
+            num2 = strtoull(end + 1, NULL, DECIMAL_BASE);
+            total += part1(num1, num2);
+            token = strtok(NULL, ",");
+        }
+
+        printf("Total: %llu\n", total);
+        #ifdef answer
+            printf("%s\n",  total == answer ? "Correct" : "Incorrect");
+        #endif
     }
-
 
     fclose(fp);
 
@@ -46,21 +56,48 @@ int main(void) {
 void remove_newline(char *buf) {
     char *str = buf;
     str = strchr(str, '\n');
+    if (!str) {
+        return;
+    }
     *str = '\0';
 }
 
-void lltostr(long long number, char *buf, size_t buf_size) {
+size_t lltostr(uint64_t number, char *buf, size_t buf_size) {
     size_t index = 0;
 
-    long long digits = number;
+    uint64_t digits = number;
     while (digits > 0) {
         index++;
         digits /= 10;
     }
+    if (index > buf_size) {
+        fprintf(stderr, "Index %zu > buffer size %zu", index, buf_size);
+        exit(1);
+    }
+    size_t length = index;
 
-    buf[index] = 0;
-    while (number > 0 && index < buf_size) {
+    buf[index] = '\0';
+    while (number > 0 && index > 0) {
         buf[--index] = '0' + (number % 10);
         number /= 10;
     }
+    return length;
+}
+
+uint64_t part1(uint64_t num1, uint64_t num2) {
+    // printf("Num1: %llu; Num2: %llu\n", num1, num2);
+    char num_str[BUFSIZ] = { 0 };
+    size_t length;
+    uint64_t total = 0;
+    for (uint64_t i = num1; i <= num2; i++) {
+        length = lltostr(i, num_str, BUFSIZ);
+        // Odd length means no symmetry
+        if (length & 1) {
+            continue;
+        }
+        if (!memcmp(num_str, &num_str[(length / 2)], length / 2)) {
+            total += i;
+        }
+    }
+    return total;
 }
